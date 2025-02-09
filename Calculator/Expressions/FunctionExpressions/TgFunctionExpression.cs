52872a9adf7386b54c4a66cd2dfb62fdd4bb34ab
Tesmi <88522894+Tesmi-Develop@users.noreply.github.com>
2025-02-09 21:45:33 +1000

    feat: added tan

---
+++ Calculator/Expressions/FunctionExpressions/TgFunctionExpression.cs
@@ -0,0 +1,17 @@
+namespace Calculator.Expressions.FunctionExpressions;
+
+[Expression]
+public class TgFunctionExpression : FunctionExpression
+{
+    protected override bool IsValidToken(string identifier)
+    {
+        return identifier.ToLower() == "tan" || identifier.ToLower() == "tg";
+    }
+
+    protected override double OnCompute(List<double> arguments)
+    {
+        if (arguments.Count == 0)
+            throw new InvalidOperationException($"At least one argument is required. {name}");
+        return Math.Tan((arguments[0] * (Math.PI)) / 180);
+    }
+}
